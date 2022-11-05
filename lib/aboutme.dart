11b78b5b07708ca0dff1088285bbdcd44f3433d6
FlutterBugs <aloaderemi1@gmail.com>
2022-11-05 11:37:25 +0100

    Added contact links and a navigation bar

--- lib/aboutme.dart
+++ lib/aboutme.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
-import 'bottom_nav_bar.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class AboutMe extends StatelessWidget {
+class AboutMe extends StatefulWidget {
   const AboutMe({super.key});
 
+  @override
+  State<AboutMe> createState() => _AboutMeState();
+}
+
+class _AboutMeState extends State<AboutMe> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,8 +24,7 @@ class AboutMe extends StatelessWidget {
               color: Colors.black, fontWeight: FontWeight.bold),
         ),
       ),
-      body: const Body(),
-      // bottomNavigationBar: CustomButtonNavBar(),
+      body: Body(),
     );
   }
 }
@@ -34,49 +39,85 @@ class Body extends StatefulWidget {
 class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-          const SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              const CircleAvatar(
-                backgroundImage: AssetImage("assets/images/picture.jpg"),
-                radius: 60,
-              ),
-              Card(
-                child: Container(
-                  padding: EdgeInsets.all(10),
-                  width: 200,
-                  height: 100,
-                  child: Text(
-                    "Hi, I'm Remi",
-                    style: GoogleFonts.roboto(
-                        fontWeight: FontWeight.bold, fontSize: 35),
-                  ),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            const CircleAvatar(
+              backgroundImage: AssetImage("assets/images/picture.jpg"),
+              radius: 60,
+            ),
+            Card(
+              child: Container(
+                padding: EdgeInsets.all(10),
+                width: double.infinity,
+                height: 280,
+                child: Text(
+                  "Hey there, I'm Remi, a budding mobile developer who also loves anime, games and reading. I also dabble in poetry from time to time."
+                  "\n \nFor the company, i would be able to translate deisgns provided into their relevent replicaitons on mobile platforms, and as I use Flutter, i would not be limited to either iOS or Android",
+                  style: GoogleFonts.roboto(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w300,
+                      fontSize: 20),
                 ),
               ),
-            ],
-          ),
-          const SizedBox(height: 50),
-          Card(
-            child: Container(
-              padding: EdgeInsets.all(10),
-              width: double.infinity,
-              height: 300,
-              child: Text(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam laoreet neque eu pulvinar venenatis. Cras eleifend erat eu vulputate interdum. Ut sagittis ac arcu id commodo. Nam sed convallis neque. Duis vitae posuere mi. Cras velit dolor, aliquam vitae convallis id, tincidunt in turpis. Maecenas fermentum lacus quis dui faucibus ultrices.",
-                style: GoogleFonts.roboto(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20),
+            ),
+            Container(
+              child: Wrap(
+                children: const [
+                  ContactInfo(
+                    url:
+                        "mailto:aloaderemi1@gmail.com?subject=Sample Subject&body=This is a Sample email",
+                    username: "aloaderemi1@gmail.com",
+                    icon: Icon(Icons.mail),
+                  ),
+                  ContactInfo(
+                      url: "https://github.com/aderemi-alo",
+                      icon: Icon(FontAwesomeIcons.github),
+                      username: "aderemi-alo"),
+                  ContactInfo(
+                      url: "https://twitter.com/alo_aderemi",
+                      icon: Icon(FontAwesomeIcons.twitter),
+                      username: "@alo_aderemi"),
+                  ContactInfo(
+                      url: "https://www.linkedin.com/in/aderemialo/",
+                      icon: Icon(FontAwesomeIcons.linkedin),
+                      username: "Aderemi Alo")
+                ],
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class ContactInfo extends StatelessWidget {
+  const ContactInfo({
+    Key? key,
+    required this.url,
+    required this.icon,
+    required this.username,
+  }) : super(key: key);
+
+  final String url;
+  final Icon icon;
+  final String username;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(
+          onPressed: () {
+            launchUrl(Uri.parse(url));
+          },
+          icon: icon,
+        ),
+        Text(username)
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:task2/aboutme.dart';
 import 'package:task2/skills.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,6 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        textTheme: TextTheme(),
         cardTheme: CardTheme(
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(15),
@@ -37,8 +40,71 @@ class MyApp extends StatelessWidget {
             color: Color.fromARGB(255, 21, 21, 21),
           ),
           textTheme: TextTheme(headlineMedium: GoogleFonts.roboto())),
-      home: SkillsPage(),
+      home: Navigation(),
       debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Navigation extends StatefulWidget {
+  const Navigation({super.key});
+
+  @override
+  State<Navigation> createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
+  static const List pages = [AboutMe(), SkillsPage()];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[_selectedIndex],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              color: Colors.black.withOpacity(.1),
+            )
+          ],
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 8),
+            child: GNav(
+              rippleColor: Colors.grey[300]!,
+              hoverColor: Colors.grey[100]!,
+              gap: 8,
+              activeColor: Colors.black,
+              iconSize: 24,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              duration: Duration(milliseconds: 400),
+              tabBackgroundColor: Colors.grey[100]!,
+              color: Colors.black,
+              tabs: [
+                GButton(
+                  icon: FontAwesomeIcons.person,
+                  text: 'About Me',
+                ),
+                GButton(
+                  icon: FontAwesomeIcons.briefcaseMedical,
+                  text: 'Portfolio',
+                ),
+              ],
+              selectedIndex: _selectedIndex,
+              onTabChange: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
